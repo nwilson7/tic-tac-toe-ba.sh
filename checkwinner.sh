0e7afc7d1679e56e14e480e983eaeac8990e2016
jaza2345 <jaza2345@gmail.com>
2025-01-09 13:14:45 +0000

    fixes syntax

--- checkwinner.sh
+++ checkwinner.sh
@@ -4,41 +4,41 @@ source checkmatch.sh
 
 check_winner()
 {
-	if [ $game_on == false ]; then
+	if [ "$game_on" == "false" ]; then
 		return;
 	fi
 	check_match 0 1 2
-        if [ $game_on == false ]; then
+        if [ "$game_on" == "false" ]; then
 		return;
 	fi
 	check_match 3 4 5
-	if [ $game_on == false ]; then
+	if [ "$game_on" == "false" ]; then
 		return;
 	fi
 	check_match 6 7 8
-        if [ $game_on == false ]; then
+        if [ "$game_on" == "false" ]; then
 		return;
 	fi
 	check_match 0 3 6
-	if [ $game_on == false ]; then
+	if [ "$game_on" == "false" ]; then
 		return;
 	fi
 	check_match 1 4 7
-        if [ $game_on == false ]; then
+        if [ "$game_on" == "false" ]; then
 		return;
 	fi
 	check_match 2 5 8
-	if [ $game_on == false ]; then
+	if [ "$game_on" == "false" ]; then
 		return;
 	fi
 	check_match 0 4 8
-	if [ $game_on == false ]; then
+	if [ "$game_on" == "false" ]; then
 		return;
 	fi
 	check_match 2 4 6
 
-	if [ $turn > 9 ]; then
-		$game_on=false
+	if [ "$turn" -gt 9 ]; then
+		game_on=false
 		echo "Game ends in a draw!"
 	fi
 }
