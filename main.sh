609cffe1b9ed1df668c182c125889933ada1a00b
jaza2345 <jaza2345@gmail.com>
2025-01-09 11:07:45 +0000

    adds player turn

--- main.sh
+++ main.sh
@@ -15,8 +15,30 @@ board () {
 	  echo "-----------"
 	  echo " ${moves[6]} | ${moves[7]} | ${moves[8]} "
 	  echo "============="
-	      }
-board
+  }
+
+player_turn () {
+	if [[ $(($turn % 2)) == 0 ]]; then
+		play=$player2
+		echo -n "Player 2, choose a square: "
+	else
+		play=$player1
+		echo -n "Player 1, choose a square: "
+	fi
+
+	read "" square
+	
+	space=${moves[$square - 1)]}
+
+	if [[ ! $space =~ ^[0-9]+$ ]]; then
+		echo "Invalid square!"
+		player_turn
+	else
+		moves[$square -1)]=$play
+		((turn++))
+	fi
+	space=${moves[($square - 1)]}
+}
 
 
 
