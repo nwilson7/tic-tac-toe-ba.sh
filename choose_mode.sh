572305c16873e5a3da9e864bffa19db074dd474b
nwilson7 <87141874+nwilson7@users.noreply.github.com>
2025-01-09 16:26:32 +0000

    removes solo from mode options

--- choose_mode.sh
+++ choose_mode.sh
@@ -3,18 +3,15 @@
 source playerturn.sh
 
 choose_mode() {
-        read -p "Choose mode(solo/multiplayer/quit):" mode
+        read -p "Choose mode(multiplayer/quit):" mode
 
 	case $mode in
-		solo)
-			:
-			;;
 		multiplayer)
 			echo "You have chosen to play multiplayer mode"
 			board
 			player_turn
 			;;
-		quit)
+		*)
 			break
 			;;
 	esac
